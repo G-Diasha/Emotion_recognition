1eff47fadd6ca75595acac17cee6025af84859ba
Diasha <ghoshdiash@gmail.com>
2026-02-26 21:47:27 +0000

    modify

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 import streamlit as st
 
+st.set_page_config(
+    page_title="Emotion Recognition",
+    page_icon="😊",
+    layout="centered"
+)
+st.title("😊 Emotion Recognition Model")
+
 uploaded_file = st.file_uploader(
     "Upload images", accept_multiple_files="directory", type=["jpg", "png"]
 )
