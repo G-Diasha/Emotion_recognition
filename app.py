3ea42cdcf9ba31397bc3f97d711d53c8560d98fe
Diasha <ghoshdiash@gmail.com>
2026-02-26 21:44:00 +0000

    modify

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ import streamlit as st
 uploaded_file = st.file_uploader(
     "Upload images", accept_multiple_files="directory", type=["jpg", "png"]
 )
-for uploaded_file in uploaded_files:
+for uploaded_file in uploaded_file:
     st.image(uploaded_file)
