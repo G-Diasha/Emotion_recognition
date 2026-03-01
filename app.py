401fc144e7a3fce1db3664f641a592f3c80a2bb1
Diasha <ghoshdiash@gmail.com>
2026-03-01 00:21:44 +0000

    app can predict

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import numpy as np
+import cv2
 from src.prediction import predict_emotion
 
 st.set_page_config(
@@ -8,15 +10,21 @@ st.set_page_config(
 )
 st.title("😊 Emotion Recognition Model")
 
+'''
 uploaded_file = st.file_uploader(
     "Upload images", accept_multiple_files="directory", type=["jpg", "png"]
 )
 for uploaded_file in uploaded_file:
     st.image(uploaded_file)
+'''
 enable = st.checkbox("Enable camera")
 picture = st.camera_input("Take a picture", disabled=not enable)
 if picture:
     st.image(picture)
-    emotion =predict_emotion(picture)
-    st.subheader("This is your emotion right now", emotion)
+    bytes_value = picture.getvalue()
+    np_array = np.frombuffer(bytes_value, np.uint8)
+    img = cv2.imdecode(np_array, cv2.IMREAD_COLOR)
+    st.image(img, channels='BGR')
+    emotion =predict_emotion(img)
+    st.markdown(emotion)
 
