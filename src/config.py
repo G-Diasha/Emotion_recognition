321d2d3a669328b51501f7aa73e2ed03a26e4e37
Diasha <ghoshdiash@gmail.com>
2026-03-08 00:58:02 +0000

    Fixing path for deployment

--- src/config.py
+++ src/config.py
@@ -1,9 +1,11 @@
+import os
 IMAGE_SIZE = (140, 140)
 BATCH_SIZE = 64
 CLASS_NAMES = [
     "angry","disgust","fear",
     "happy","neutral","sad","surprise"
 ]
-MODEL_PATH = "C:/Users/User/Desktop/emotion/models/resnet50_model2.keras"
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+MODEL_PATH = os.path.join(BASE_DIR, "models", "resnet50_model2.keras")
 DATA_DIR = "c:\\Users\\User\\Desktop\\emotion\\dataset\\train"
 
--- src/model_loader.py
+++ src/model_loader.py
@@ -5,3 +5,4 @@ from .config import MODEL_PATH
 def load_model():
     model = tf.keras.models.load_model(MODEL_PATH, compile=False)
     return model
+#MODEL_PATH = "C:/Users/User/Desktop/emotion/models/resnet50_model2.keras"
